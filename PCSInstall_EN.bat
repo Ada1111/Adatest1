56487507953d3ac82be006b845546bd413f4650c
Ada1111 <43163275+Ada1111@users.noreply.github.com>
2018-09-12 15:18:07 +0800

    Update PCSInstall_EN.bat

--- PCSInstall_EN.bat
+++ PCSInstall_EN.bat
@@ -1,5 +1,2 @@
-//IF EXIST "C:\buildpath\"(@RD /S /Q C:\buildpath\)
-
-//Xcopy %buildpath%\DVD_PC C:\buildpath\ /s /e /y
 Xcopy \\mydph0393.hpeswlab.net\PC\Build\configfile\PCS  C:\configpath\ /s /e /y
-msiexec.exe /i "C:\buildpath\Setup\Install\Server\PCServer.msi" USER_CONFIG_FILE_PATH="C:\configpath\UserInput.xml" /qn /l*vx "C:\install.log"
+msiexec.exe /i "C:\Users\CSSG_LnG_Auto\Desktop\MF_PC_1260_English_LP\Setup\Install\Server\PCServer.msi" USER_CONFIG_FILE_PATH="C:\configpath\UserInput.xml" /qn /l*vx "C:\install.log"
