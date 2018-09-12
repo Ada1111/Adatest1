5b163f589b8c1cdfb1673359456ef68d506935e9
Ada1111 <43163275+Ada1111@users.noreply.github.com>
2018-09-12 15:37:37 +0800

    Update PCS-EN-install.bat

--- PCS/PCS-EN-install.bat
+++ PCS/PCS-EN-install.bat
@@ -1,1 +1,3 @@
 
+Xcopy \\mydph0393.hpeswlab.net\PC\Build\configfile\PCS  C:\configpath\ /s /e /y
+msiexec.exe /i "C:\Users\CSSG_LnG_Auto\Desktop\DVD\Setup\Install\Server\PCServer.msi" USER_CONFIG_FILE_PATH="C:\configpath\UserInput.xml" /qn /l*vx "C:\install.log"
